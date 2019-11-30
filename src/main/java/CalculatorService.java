26dbfba80ddb6bfbee1c08dea5cee882ce5e8909
David <mcrobert.david@gmail.com>
2019-11-30 17:00:03 +0000

    Clicked on the java folder then Mark Directory As -> Source Root. This allows the system to identfy the components as being runable.

--- src/main/java/CalculatorService.java
+++ src/main/java/CalculatorService.java
@@ -1,4 +1,4 @@
-package java;
+
 
 public interface CalculatorService {
     public double add(double input1, double input2);
--- src/main/java/MathApplication.java
+++ src/main/java/MathApplication.java
@@ -1,4 +1,4 @@
-package java;
+
 
 public class MathApplication {
         private CalculatorService calcService;
